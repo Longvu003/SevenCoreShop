fe2f33d8efe7a04fa43954d6072b1f9b602b3ac6
Longvu003 <Hailongvu552@gmail.com>
2024-10-18 11:16:23 +0700

    upcode App Backup full

---
+++ SevenCoreShop/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import AppNavigation from './navigation/AppNavigation';
+import Signin from './src/login/Signin';
+import Create from './src/login/Create';
+import Forgot from './src/login/Forgot';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <AppNavigation />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Đặt flex: 1 để chiếm toàn bộ không gian màn hình
+  },
+});
