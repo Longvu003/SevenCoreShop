530faf5b6e6403e93fd19d2839710489a32bee89
Trịnh Công Huy <ryton2811@gmail.com>
2024-11-01 22:16:20 +0700

    Giai quyet xung dot

--- SevenCoreShop_Backup2/App.tsx
+++ SevenCoreShop_Backup2/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Stacknavigation from './src/Navigation/Stacknavigation';
 import store from './src/ReduxTookit/Store';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 
 
 
 const App = () => {
   return (
-    <Provider store={store}>
+    <View style = {{flex : 1}}>
       <Stacknavigation />
     
-    </Provider>
+    </View>
   );
 };
 
--- SevenCoreShop_Backup2/src/Navigation/Stacknavigation.js
+++ SevenCoreShop_Backup2/src/Navigation/Stacknavigation.js
@@ -24,7 +24,7 @@ const Stacknavigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Tab"
+        initialRouteName="SignupScreen"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="Welcome1" component={Welcome1} />
         <Stack.Screen name="Welcome2" component={Welcome2} />
