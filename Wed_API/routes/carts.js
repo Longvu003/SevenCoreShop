ba92d77403a1d4f7be6cf4df7affb79ca2a58df9
Longvu003 <Hailongvu552@gmail.com>
2024-11-20 15:20:33 +0700

    up code master fake

--- Wed_API/routes/carts.js
+++ Wed_API/routes/carts.js
@@ -1,12 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-
-
 const cartController = require("../controllers/CartController");
-
-
-
 
 /**
  * Thêm sản phẩm vào giỏ hàng
@@ -18,15 +13,22 @@ const cartController = require("../controllers/CartController");
 router.post("/addItemcart", async (req, res) => {
   try {
     console.log("Dữ liệu nhận được từ client:", req.body);
-    const { userId, productId, nameProduct, quantity, price, images } = req.body;
-    const result = await cartController.add(userId, productId, nameProduct, quantity, price, images);
+    const { userId, productId, nameProduct, quantity, price, images } =
+      req.body;
+    const result = await cartController.add(
+      userId,
+      productId,
+      nameProduct,
+      quantity,
+      price,
+      images
+    );
     return res.status(200).json({ message: "Thêm thành công", result });
   } catch (error) {
     console.error("Lỗi trong route /addItemcart:", error.stack);
     return res.status(500).json({ status: false, data: error.message });
   }
 });
-
 
 /**
  * Lấy toàn bộ sản phẩm trong giỏ hàng của người dùng
@@ -39,7 +41,10 @@ router.get("/getItemCartById", async (req, res) => {
   try {
     const userId = req.query.id;
     const result = await cartController.getItemCart(userId);
-    const totalPrice = result.reduce((total, item) => total + item.price * item.quantity, 0);
+    const totalPrice = result.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
     return res.status(200).json({ status: true, data: totalPrice, result });
   } catch (error) {
     return res.status(500).json({ status: false, data: error.message });
@@ -85,7 +90,11 @@ router.delete("/deleteItemCart", async (req, res) => {
 router.put("/updateItemCart", async (req, res) => {
   const { userId, productId, quantity } = req.body;
   try {
-    const { success, item, message } = await cartController.updateItemCart(userId, productId, quantity);
+    const { success, item, message } = await cartController.updateItemCart(
+      userId,
+      productId,
+      quantity
+    );
 
     if (!success) {
       return res.status(404).json({ message });
@@ -94,21 +103,25 @@ router.put("/updateItemCart", async (req, res) => {
     // Nếu cập nhật thành công, tính lại tổng giá của giỏ hàng
     const cart = await cartController.getItemCart(userId);
     if (!cart || cart.length === 0) {
-      return res.status(404).json({ message: "Giỏ hàng trống hoặc không tồn tại", totalPrice: 0 });
+      return res
+        .status(404)
+        .json({ message: "Giỏ hàng trống hoặc không tồn tại", totalPrice: 0 });
     }
 
-    const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
+    const totalPrice = cart.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
 
-    return res.status(200).json({ message: "Update thành công", item, totalPrice });
+    return res
+      .status(200)
+      .json({ message: "Update thành công", item, totalPrice });
   } catch (error) {
     console.log("Error updating cart:", error);
     res.status(500).json({ status: false, data: error.message });
   }
 });
 
-
-
-router.post('/checkout', cartController.checkout);
-
+router.post("/checkout", cartController.checkout);
 
 module.exports = router;
