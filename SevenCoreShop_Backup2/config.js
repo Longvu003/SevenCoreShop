15cb0ef04c79b7f3ff580fd3e958f3a7d3e9eb63
NguyenTanTai <taintps31384@fpt.edu.vn>
2024-12-14 22:50:16 +0700

    delete code nhanh main

--- SevenCoreShop_Backup2/config.js
+++
@@ -1,2 +0,0 @@
-const API__URL = 'http://192.168.1.3:7777';
-export default API__URL;
--- SevenCoreShop_Backup2/src/ReduxTookit/Store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import CartSlice from '../ReduxTookit/Cartslice';
-const store = configureStore({
-  reducer: {
-    Cart: CartSlice,
-  },
-});
-
-export default store;
