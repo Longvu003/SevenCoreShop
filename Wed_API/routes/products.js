fa392afc2222870339217e0b6adc932dca8e582e
NguyenTanTai <taintps31384@fpt.edu.vn>
2024-12-15 01:24:43 +0700

    merd code của Tài Đep Zai Code Dở

---
+++ Wed_API/routes/products.js
@@ -0,0 +1,130 @@
+var express = require('express');
+var router = express.Router();
+// http://localhost:7777/products
+
+const ProductController = require('../controllers/ProductController')
+
+/**
+ * API cập nhật trạng thái available của sản phẩm
+ * method: post
+ * url: http://localhost:7777/products/:id/availability
+ * body: { available }
+ * response: trả về sản phẩm vừa cập nhật
+ */
+router.post('/:id/availability', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { avaialble } = req.body;
+        console.log(id, avaialble)    
+        const product = await ProductController.updateProductAvailability(id, avaialble);
+        return res.status(200).json({ status: true, data: product });
+    } catch (error) {
+        return res.status(500).json({ status: false, data: error.message });
+    }
+});
+
+
+router.get('/', async (req, res, next) => {
+    const { category_id } = req.query
+    console.log(category_id)
+    try {
+        const checkListProducts = await ProductController.getProducts(category_id);
+        return res.status(200).json({ status: true, data: checkListProducts });
+    } catch (error) {
+        return res.status(500).json({ status: false, data: error.message });
+    }
+});
+
+
+/**
+ * method: post
+ * url: http://localhost:7777/products
+ * body: {name, price, quantity, images, decription, category}
+ * response: trả về sản phẩm vừa tạo
+ */
+router.post('/', async (req, res, next) => {
+    try {
+        const { name, price, quantity, images, description, category } = req.body;
+        const product = await ProductController.addProduct(name, price, quantity, images, description, category);
+        return res.status(200).json({ status: true, data: product })
+    } catch (error) {
+        return res.status(500).json({ status: false, data: error.message })
+    }
+});
+
+/**
+ * API cập nhật sản phẩn 
+ * method: post
+ * url: http://localhost:7777/products/:id/update
+ * body: name, price, quantity, images, description, category
+ * response: trå về sån phåm vừa cập nhật
+ */
+router.post('/:id/update', async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const { name, price, quantity, images, description, category } = req.body;
+        console.log(req.body)
+        // console.log(category)
+        const product = await ProductController.updateProduct(id, name, price, quantity, images, description, category.category_id);
+        return res.status(200).json({ status: true, data: product });
+    } catch (error) {
+        return res.status(500).json({ status: false, data: error.message });
+    }
+})
+
+
+
+
+//API lấy danh sách sản phẩm theo category_id
+// method: get
+// url: http://localhost:7777/products/danh-muc?id=1
+// kết quả: danh sách sản phẩm theo danh mục
+router.get('/category', async (req, res, next) => {
+    try {
+        const { id } = req.query;
+        const products = await ProductController.getProductByCategory(id);
+        return res.status(200).json({ status: true, data: products })
+    } catch (error) {
+        return res.status(500).json({ status: false, data: error.message })
+    }
+});
+
+
+
+/**
+ * API Xóa SP
+ * method: post
+ * url: http://localhost:7777/products/:id/delete
+ * response: trå về sån phåm vừa cập nhật
+ */
+router.post('/:id/delete', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const products = await ProductController.deleteProduct(id);
+        return res.status(200).json({ status: true, data: products })//
+    } catch (error) {
+        return res.status(500).json({ status: false, data: error.message })
+    }
+});
+
+
+
+/**
+ * API Lấy sp theo id
+ * method: get
+ * url: http://localhost:7777/products/:id
+ * response: trå về sån phåm vừa cập nhật
+ */
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log(id);
+        const products = await ProductController.getById(id);
+        return res.status(200).json({ status: true, data: products })
+    } catch (error) {
+        return res.status(500).json({ status: false, data: error.message })
+    }
+});
+
+
+module.exports = router;
---
+++ admin/src/service/ProducService.ts
@@ -0,0 +1,126 @@
+import { Products } from '../model/ProductModel';
+const API_URL = 'http://localhost:7777'; // Cập nhật URL chính xác
+
+
+export const GetProduct = async (): Promise<Products> => {
+  const response = await fetch(`${API_URL}/products`, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: any = await response.json();
+
+  if (!response.ok) {
+    return data
+  }
+
+  return data;
+}
+
+//creat new product
+export const CreateProduct = async (product: Products): Promise<Products> => {
+  const response = await fetch(`${API_URL}/products`, {
+    method: "post",
+    body: JSON.stringify(product),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: any = await response.json();
+
+  if (!response.ok) {
+    return data
+  }
+
+  return data;
+}
+
+export const DeleteProduct = async (id: string): Promise<Products> => {
+  const response = await fetch(`${API_URL}/products/${id}/delete`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: any = await response.json();
+
+  if (!response.ok) {
+    return data
+  }
+
+  return data;
+}
+
+// get product by id
+export const GetProductById = async (id: string): Promise<Products> => {
+  const response = await fetch(`${API_URL}/products/${id}`, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json(); 
+    throw new Error(errorData.message || 'Failed to fetch product'); 
+  }
+
+  const data: Products = await response.json();
+  return data; 
+}
+
+
+export const EditProductByid = async (id: string, product: Products): Promise<Products> => {
+  const response = await fetch(`${API_URL}/products/${id}/update`, {
+    method: "post",
+    body: JSON.stringify(product),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: any = await response.json();
+
+  if (!response.ok) {
+    return data
+  }
+
+  return data;
+}
+
+export const GetProductByCategoryId = async (id: string): Promise<Products[]> => {
+  try {
+      const response = await fetch(`${API_URL}/products?categoryId=${id}`, {
+          method: "get",
+          headers: {
+              "Content-Type": "application/json",
+          },
+      });
+      const data = await response.json();
+      if (!response.ok) {
+          return []; 
+      }
+      return data.products || []; 
+  } catch (error) {
+      console.error('Failed to fetch products by category ID:', error);
+      return []; 
+  }
+}
+
+// Update product availability
+export const UpdateProductAvailability = async (id: string, avaialble: boolean): Promise<Products> => {
+  const response = await fetch(`${API_URL}/products/${id}/availability`, {
+    method: "post",
+    body: JSON.stringify({ avaialble }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: any = await response.json();
+
+  if (!response.ok) {
+    return data;
+  }
+
+  return data;
+};
