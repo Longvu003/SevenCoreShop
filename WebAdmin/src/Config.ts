8d796f61647509eaaececafcd64bb6ef0f6b472a
Longvu003 <Hailongvu552@gmail.com>
2024-12-17 10:17:46 +0700

    .

--- WebAdmin/src/Config.ts
+++
@@ -1,2 +0,0 @@
-const API_URL = "http://localhost:7777"
-export default API_URL
--- WebAdmin/src/model/CategoriesModel.ts
+++
@@ -1,6 +0,0 @@
-export interface Category {
-    _id: string;
-    name: string;
-    description: string;
-    images: string[]; // Add this line
-}
